8654d80068c117cf0f85bca5acbcba83be76ac39
sangkim99 <rettsadd990922@kookmin.ac.kr>
2023-12-26 16:47:40 +0900

    springboot 프로젝트 시작

---
+++ src/main/java/com/example/team10/Team10Application.java
@@ -0,0 +1,13 @@
+package com.example.team10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team10Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Team10Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/team10/Team10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.team10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team10ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
